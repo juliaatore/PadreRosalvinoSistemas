1414228b050f1eb3f55c559aedee1b527e2332ab
juatore <159203072+juliaatore@users.noreply.github.com>
2024-08-26 21:41:09 -0300

    Add files via upload

---
+++ Atividade Rosalvino/Projeto Rosalvino/Projeto Rosalvino/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿
+namespace Projeto_Rosalvino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Grp_Temas = new System.Windows.Forms.GroupBox();
+            this.Btn_Nome = new System.Windows.Forms.Button();
+            this.Btn_Sobrenome = new System.Windows.Forms.Button();
+            this.Btn_Idade = new System.Windows.Forms.Button();
+            this.Btn_Bairro = new System.Windows.Forms.Button();
+            this.Btn_celular = new System.Windows.Forms.Button();
+            this.Btn_Email = new System.Windows.Forms.Button();
+            this.Btn_Dados = new System.Windows.Forms.Button();
+            this.Txt_Name1 = new System.Windows.Forms.TextBox();
+            this.Text_Name2 = new System.Windows.Forms.TextBox();
+            this.Text_Name3 = new System.Windows.Forms.TextBox();
+            this.Txt_Name4 = new System.Windows.Forms.TextBox();
+            this.Txt_Name5 = new System.Windows.Forms.TextBox();
+            this.Txt_Name6 = new System.Windows.Forms.TextBox();
+            this.Lab_Nome = new System.Windows.Forms.Label();
+            this.Lab_Sobrenome = new System.Windows.Forms.Label();
+            this.Lab_Idade = new System.Windows.Forms.Label();
+            this.Lab_Bairro = new System.Windows.Forms.Label();
+            this.Lab_Celular = new System.Windows.Forms.Label();
+            this.Lab_Email = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Rad_Padrao = new System.Windows.Forms.RadioButton();
+            this.Rad_Tema1 = new System.Windows.Forms.RadioButton();
+            this.Rad_Tema2 = new System.Windows.Forms.RadioButton();
+            this.Btn_Ativar = new System.Windows.Forms.Button();
+            this.Btn_Desativar = new System.Windows.Forms.Button();
+            this.Btn_Limpar = new System.Windows.Forms.Button();
+            this.Grp_Temas.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Grp_Temas
+            // 
+            this.Grp_Temas.BackColor = System.Drawing.Color.Transparent;
+            this.Grp_Temas.Controls.Add(this.Rad_Tema2);
+            this.Grp_Temas.Controls.Add(this.Rad_Tema1);
+            this.Grp_Temas.Controls.Add(this.Rad_Padrao);
+            this.Grp_Temas.Location = new System.Drawing.Point(28, 339);
+            this.Grp_Temas.Name = "Grp_Temas";
+            this.Grp_Temas.Size = new System.Drawing.Size(205, 99);
+            this.Grp_Temas.TabIndex = 0;
+            this.Grp_Temas.TabStop = false;
+            this.Grp_Temas.Text = "Temas";
+            // 
+            // Btn_Nome
+            // 
+            this.Btn_Nome.Location = new System.Drawing.Point(490, 85);
+            this.Btn_Nome.Name = "Btn_Nome";
+            this.Btn_Nome.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Nome.TabIndex = 1;
+            this.Btn_Nome.Text = "NOME";
+            this.Btn_Nome.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Sobrenome
+            // 
+            this.Btn_Sobrenome.Location = new System.Drawing.Point(490, 123);
+            this.Btn_Sobrenome.Name = "Btn_Sobrenome";
+            this.Btn_Sobrenome.Size = new System.Drawing.Size(86, 23);
+            this.Btn_Sobrenome.TabIndex = 2;
+            this.Btn_Sobrenome.Text = "SOBRENOME";
+            this.Btn_Sobrenome.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Idade
+            // 
+            this.Btn_Idade.Location = new System.Drawing.Point(490, 165);
+            this.Btn_Idade.Name = "Btn_Idade";
+            this.Btn_Idade.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Idade.TabIndex = 3;
+            this.Btn_Idade.Text = "IDADE";
+            this.Btn_Idade.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Bairro
+            // 
+            this.Btn_Bairro.Location = new System.Drawing.Point(490, 205);
+            this.Btn_Bairro.Name = "Btn_Bairro";
+            this.Btn_Bairro.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Bairro.TabIndex = 4;
+            this.Btn_Bairro.Text = "BAIRRO";
+            this.Btn_Bairro.UseVisualStyleBackColor = true;
+            // 
+            // Btn_celular
+            // 
+            this.Btn_celular.Location = new System.Drawing.Point(490, 243);
+            this.Btn_celular.Name = "Btn_celular";
+            this.Btn_celular.Size = new System.Drawing.Size(75, 23);
+            this.Btn_celular.TabIndex = 5;
+            this.Btn_celular.Text = "CELULAR";
+            this.Btn_celular.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Email
+            // 
+            this.Btn_Email.Location = new System.Drawing.Point(490, 287);
+            this.Btn_Email.Name = "Btn_Email";
+            this.Btn_Email.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Email.TabIndex = 6;
+            this.Btn_Email.Text = "E-MAIL";
+            this.Btn_Email.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Dados
+            // 
+            this.Btn_Dados.Location = new System.Drawing.Point(375, 329);
+            this.Btn_Dados.Name = "Btn_Dados";
+            this.Btn_Dados.Size = new System.Drawing.Size(83, 47);
+            this.Btn_Dados.TabIndex = 7;
+            this.Btn_Dados.Text = "DADOS COMPLETO";
+            this.Btn_Dados.UseVisualStyleBackColor = true;
+            // 
+            // Txt_Name1
+            // 
+            this.Txt_Name1.Location = new System.Drawing.Point(375, 85);
+            this.Txt_Name1.Name = "Txt_Name1";
+            this.Txt_Name1.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Name1.TabIndex = 8;
+            // 
+            // Text_Name2
+            // 
+            this.Text_Name2.Location = new System.Drawing.Point(375, 122);
+            this.Text_Name2.Name = "Text_Name2";
+            this.Text_Name2.Size = new System.Drawing.Size(100, 20);
+            this.Text_Name2.TabIndex = 9;
+            // 
+            // Text_Name3
+            // 
+            this.Text_Name3.Location = new System.Drawing.Point(375, 165);
+            this.Text_Name3.Name = "Text_Name3";
+            this.Text_Name3.Size = new System.Drawing.Size(100, 20);
+            this.Text_Name3.TabIndex = 10;
+            // 
+            // Txt_Name4
+            // 
+            this.Txt_Name4.Location = new System.Drawing.Point(375, 205);
+            this.Txt_Name4.Name = "Txt_Name4";
+            this.Txt_Name4.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Name4.TabIndex = 11;
+            // 
+            // Txt_Name5
+            // 
+            this.Txt_Name5.Location = new System.Drawing.Point(375, 245);
+            this.Txt_Name5.Name = "Txt_Name5";
+            this.Txt_Name5.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Name5.TabIndex = 12;
+            // 
+            // Txt_Name6
+            // 
+            this.Txt_Name6.Location = new System.Drawing.Point(375, 287);
+            this.Txt_Name6.Name = "Txt_Name6";
+            this.Txt_Name6.Size = new System.Drawing.Size(100, 20);
+            this.Txt_Name6.TabIndex = 13;
+            // 
+            // Lab_Nome
+            // 
+            this.Lab_Nome.AutoSize = true;
+            this.Lab_Nome.Location = new System.Drawing.Point(286, 85);
+            this.Lab_Nome.Name = "Lab_Nome";
+            this.Lab_Nome.Size = new System.Drawing.Size(39, 13);
+            this.Lab_Nome.TabIndex = 14;
+            this.Lab_Nome.Text = "NOME";
+            this.Lab_Nome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Lab_Sobrenome
+            // 
+            this.Lab_Sobrenome.AutoSize = true;
+            this.Lab_Sobrenome.Location = new System.Drawing.Point(289, 125);
+            this.Lab_Sobrenome.Name = "Lab_Sobrenome";
+            this.Lab_Sobrenome.Size = new System.Drawing.Size(76, 13);
+            this.Lab_Sobrenome.TabIndex = 15;
+            this.Lab_Sobrenome.Text = "SOBRENOME";
+            // 
+            // Lab_Idade
+            // 
+            this.Lab_Idade.AutoSize = true;
+            this.Lab_Idade.Location = new System.Drawing.Point(308, 174);
+            this.Lab_Idade.Name = "Lab_Idade";
+            this.Lab_Idade.Size = new System.Drawing.Size(40, 13);
+            this.Lab_Idade.TabIndex = 16;
+            this.Lab_Idade.Text = "IDADE";
+            this.Lab_Idade.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Lab_Bairro
+            // 
+            this.Lab_Bairro.AutoSize = true;
+            this.Lab_Bairro.Location = new System.Drawing.Point(308, 214);
+            this.Lab_Bairro.Name = "Lab_Bairro";
+            this.Lab_Bairro.Size = new System.Drawing.Size(48, 13);
+            this.Lab_Bairro.TabIndex = 17;
+            this.Lab_Bairro.Text = "BAIRRO";
+            // 
+            // Lab_Celular
+            // 
+            this.Lab_Celular.AutoSize = true;
+            this.Lab_Celular.Location = new System.Drawing.Point(309, 259);
+            this.Lab_Celular.Name = "Lab_Celular";
+            this.Lab_Celular.Size = new System.Drawing.Size(56, 13);
+            this.Lab_Celular.TabIndex = 18;
+            this.Lab_Celular.Text = "CELULAR";
+            this.Lab_Celular.Click += new System.EventHandler(this.Lab_Celular_Click);
+            // 
+            // Lab_Email
+            // 
+            this.Lab_Email.AutoSize = true;
+            this.Lab_Email.Location = new System.Drawing.Point(309, 287);
+            this.Lab_Email.Name = "Lab_Email";
+            this.Lab_Email.Size = new System.Drawing.Size(42, 13);
+            this.Lab_Email.TabIndex = 19;
+            this.Lab_Email.Text = "E-MAIL";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(412, 53);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "DADOS PESSOAIS";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(136, 188);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "confirmação de Dados";
+            // 
+            // Rad_Padrao
+            // 
+            this.Rad_Padrao.AutoSize = true;
+            this.Rad_Padrao.Location = new System.Drawing.Point(7, 20);
+            this.Rad_Padrao.Name = "Rad_Padrao";
+            this.Rad_Padrao.Size = new System.Drawing.Size(59, 17);
+            this.Rad_Padrao.TabIndex = 0;
+            this.Rad_Padrao.Text = "Padrão";
+            this.Rad_Padrao.UseVisualStyleBackColor = true;
+            this.Rad_Padrao.CheckedChanged += new System.EventHandler(this.Rad_Padrao_CheckedChanged);
+            // 
+            // Rad_Tema1
+            // 
+            this.Rad_Tema1.AutoSize = true;
+            this.Rad_Tema1.Location = new System.Drawing.Point(7, 44);
+            this.Rad_Tema1.Name = "Rad_Tema1";
+            this.Rad_Tema1.Size = new System.Drawing.Size(58, 17);
+            this.Rad_Tema1.TabIndex = 1;
+            this.Rad_Tema1.Text = "Tema1";
+            this.Rad_Tema1.UseVisualStyleBackColor = true;
+            this.Rad_Tema1.CheckedChanged += new System.EventHandler(this.Rad_Tema1_CheckedChanged);
+            // 
+            // Rad_Tema2
+            // 
+            this.Rad_Tema2.AutoSize = true;
+            this.Rad_Tema2.Location = new System.Drawing.Point(7, 68);
+            this.Rad_Tema2.Name = "Rad_Tema2";
+            this.Rad_Tema2.Size = new System.Drawing.Size(58, 17);
+            this.Rad_Tema2.TabIndex = 2;
+            this.Rad_Tema2.Text = "Tema2";
+            this.Rad_Tema2.UseVisualStyleBackColor = true;
+            this.Rad_Tema2.CheckedChanged += new System.EventHandler(this.Rad_Tema2_CheckedChanged);
+            // 
+            // Btn_Ativar
+            // 
+            this.Btn_Ativar.Location = new System.Drawing.Point(35, 13);
+            this.Btn_Ativar.Name = "Btn_Ativar";
+            this.Btn_Ativar.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Ativar.TabIndex = 22;
+            this.Btn_Ativar.Text = "button8";
+            this.Btn_Ativar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Desativar
+            // 
+            this.Btn_Desativar.Location = new System.Drawing.Point(139, 12);
+            this.Btn_Desativar.Name = "Btn_Desativar";
+            this.Btn_Desativar.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Desativar.TabIndex = 23;
+            this.Btn_Desativar.Text = "button9";
+            this.Btn_Desativar.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Limpar
+            // 
+            this.Btn_Limpar.Location = new System.Drawing.Point(242, 12);
+            this.Btn_Limpar.Name = "Btn_Limpar";
+            this.Btn_Limpar.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Limpar.TabIndex = 24;
+            this.Btn_Limpar.Text = "button10";
+            this.Btn_Limpar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Btn_Limpar);
+            this.Controls.Add(this.Btn_Desativar);
+            this.Controls.Add(this.Btn_Ativar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Lab_Email);
+            this.Controls.Add(this.Lab_Celular);
+            this.Controls.Add(this.Lab_Bairro);
+            this.Controls.Add(this.Lab_Idade);
+            this.Controls.Add(this.Lab_Sobrenome);
+            this.Controls.Add(this.Lab_Nome);
+            this.Controls.Add(this.Txt_Name6);
+            this.Controls.Add(this.Txt_Name5);
+            this.Controls.Add(this.Txt_Name4);
+            this.Controls.Add(this.Text_Name3);
+            this.Controls.Add(this.Text_Name2);
+            this.Controls.Add(this.Txt_Name1);
+            this.Controls.Add(this.Btn_Dados);
+            this.Controls.Add(this.Btn_Email);
+            this.Controls.Add(this.Btn_celular);
+            this.Controls.Add(this.Btn_Bairro);
+            this.Controls.Add(this.Btn_Idade);
+            this.Controls.Add(this.Btn_Sobrenome);
+            this.Controls.Add(this.Btn_Nome);
+            this.Controls.Add(this.Grp_Temas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Grp_Temas.ResumeLayout(false);
+            this.Grp_Temas.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox Grp_Temas;
+        private System.Windows.Forms.Button Btn_Nome;
+        private System.Windows.Forms.Button Btn_Sobrenome;
+        private System.Windows.Forms.Button Btn_Idade;
+        private System.Windows.Forms.Button Btn_Bairro;
+        private System.Windows.Forms.Button Btn_celular;
+        private System.Windows.Forms.Button Btn_Email;
+        private System.Windows.Forms.Button Btn_Dados;
+        private System.Windows.Forms.TextBox Txt_Name1;
+        private System.Windows.Forms.TextBox Text_Name2;
+        private System.Windows.Forms.TextBox Text_Name3;
+        private System.Windows.Forms.TextBox Txt_Name4;
+        private System.Windows.Forms.TextBox Txt_Name5;
+        private System.Windows.Forms.TextBox Txt_Name6;
+        private System.Windows.Forms.Label Lab_Nome;
+        private System.Windows.Forms.Label Lab_Sobrenome;
+        private System.Windows.Forms.Label Lab_Idade;
+        private System.Windows.Forms.Label Lab_Bairro;
+        private System.Windows.Forms.Label Lab_Celular;
+        private System.Windows.Forms.Label Lab_Email;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton Rad_Tema2;
+        private System.Windows.Forms.RadioButton Rad_Tema1;
+        private System.Windows.Forms.RadioButton Rad_Padrao;
+        private System.Windows.Forms.Button Btn_Ativar;
+        private System.Windows.Forms.Button Btn_Desativar;
+        private System.Windows.Forms.Button Btn_Limpar;
+    }
+}
+
---
+++ Atividade Rosalvino/Projeto Rosalvino/Projeto Rosalvino/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Rosalvino
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rad_Padrao_CheckedChanged(object sender, EventArgs e)
+        {
+            BackgroundImage = Properties.Resources.Alunos;
+        }
+
+        private void Rad_Tema1_CheckedChanged(object sender, EventArgs e)
+        {
+            BackgroundImage = Properties.Resources.Aluno;
+        }
+
+        private void Rad_Tema2_CheckedChanged(object sender, EventArgs e)
+        {
+            BackgroundImage = Properties.Resources.Rosalvino;
+        }
+
+        private void Lab_Celular_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
